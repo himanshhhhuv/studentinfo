fe832eb66df5787b0e8dc03bcac6ef069700a7c0
himanshhhhuv <himanshhhhuv@gmail.com>
2024-09-16 00:12:15 +0530

    lame me

---
+++ app/studentdetails/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentDetailsPage = () => {
+  return (
+    <div>
+      <h1>Student Details</h1>
+      {/* Add your student details content here */}
+    </div>
+  );
+};
+
+export default StudentDetailsPage;
